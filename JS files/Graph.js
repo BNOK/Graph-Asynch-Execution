c7af9491488d92f6801dd06e0b2413f2f19b5554
BNOK <52835202+BNOK@users.noreply.github.com>
2020-12-01 23:35:53 +0100

    Upload 04
    debugging the code and reformuling it .
    code works until finding the starting points of the graph , still debugging the function that gets the layers of the graph .

--- JS files/Graph.js
+++ JS files/Graph.js
@@ -4,72 +4,100 @@ class Graph {
     // adjacent list
     constructor(noOfVertices) {
       this.noOfVertices = noOfVertices;
-      this.GraphList = new Map();
+      this.graphList = new Map();
 
       
     }
   
-  
-    AddVertex(vertex) {
-        this.GraphList.set(v, []);
+    // adding vertex as key to a Map 
+    AddVertex(vertex){
+        this.graphList.set(vertex, []);
+    }  
       
-    }
-  
+    
+    // adding edges w as the value of the key v 
     AddEdge(v,w) {
-        this.GaphList.get(v).push(w);
+        this.graphList.get(v).push(w);
     }
+
     // find starting points ---------
+    X= new Array();
     FindStart(){
-        temp = Array.from(this.GraphList.keys());
-        this.GraphList.forEach(finding(element));
-        return temp;
-    }
-    finding(element){
-        arr1 =Array.from(element.values());
-        for(let i=0 ; i<arr1;i++){
-            if (temp.includes(arr1[i])){
-                temp.splice(i,1);
+        let temp = Array.from(this.graphList.keys()); // get the keys(vertecies) in the form of array
+        console.log("origin =" +temp);
+        this.graphList.forEach(values => {
+            const arr1 =values;
+           
+            if (arr1 !=[]){
+               
+                for(let i=0 ; i<arr1.length;i++){
+                    
+                    if (temp.includes(arr1[i])){
+                        let pos = temp.indexOf(arr1[i]);        
+                        temp.splice(pos,1);
+                        //console.log("temp splice = "+temp);
+                    }
+                }
+                
             }
-        }
+            
+        }); // loop through it 
+       
+        
+        return temp;                               // return last version of the list temp 
     }
+
+    
+
     // find all layers ---------------------
-    Looper(temp){
-        finalList = new Array();
-        finalList.push(new Set(temp));
-        index =0;
-        FinalLayer(temp){
-            index += 1;
-            arr1 = temp.forEach(findnemo);
-            temp = Array.from(finalList[index]);
-            if (temp== [])
-                    return ;
-            else
-                this.FinalLayer(temp);
-        }
+    
+    Looper(stArray){                               // wrapper function 
+        const layersList = new Array();
+        let index =0;
+
+        layersList.push(new Set(stArray));
+        console.log("fl = " + layersList[0]);
+
+        return this.FinalLayer(stArray,index,layersList);
+    }
+
+    FinalLayer(sta,index,fl){                           // recursive function (custom bfs)
+        index += 1;
+        fl.push(new Set());
+        sta.forEach(element => {
+            const arr2 = new Array(this.graphList[element]);
+            arr2.forEach(item => fl[index].add(item));
+        });
+        sta = Array.from(fl[index]);
+        if (sta == [])
+            return fl;
+        else
+            this.FinalLayer(sta);
+
 
     }
     
 
-    findnemo(element){
+    // findnemo(element){                          // call back function for the foreach
         
-        arr = this.GraphList[element];
-        S= new Set(arr);
-        finalList[index].add(S);
-    }
+    //     arr = this.graphList[element];
+    //     S= new Set(arr);
+    //     finalList[index].add(S);
+    // }
 
 
-    //----------------------------------------------------------------------------------
+    //--------------------print the Graph----------------------------------------
     printGraph()
     {
         // get all the vertices
-        var get_keys = this.AdjList.keys();
+        var get_keys = this.graphList.keys();
  
         // iterate over the vertices
         for (var i of get_keys) 
         {
             // great the corresponding adjacency list
             // for the vertex
-            var get_values = this.AdjList.get(i);
+            var get_values = this.graphList.get(i);
             var conc = "";
  
             // iterate over the adjacency list
@@ -81,6 +109,16 @@ class Graph {
             console.log(i + " -> " + conc);
         }
     }
+
+    //-----------------print starting points-----------
+    PrintStarters(x){
+        for(let i=0;i<x.length;i++){
+            console.log(`temp[${i}] = ${x[i]}`);
+        }
+    }
 }
+
+
+
 
 module.exports = Graph;
--- JS files/index.js
+++ JS files/index.js
@@ -1,17 +1,42 @@
 const GP = require('./Graph');
 
-const g = new GP(5);
+// specify the number of vertecies 
+let Graph = new GP(5);
 
-for (let i = 0; i < g.noOfVertices; i++) {
-  // take input x 
-  g.AddVertex(i);
-  // console.log(g.vertexList[i]);
+// add the vertecies
+for (let i =0; i<=10 ; i++){
+  Graph.AddVertex(i);
 }
 
-for (let i=0;i<g.vertexList.length ; i++){
-  g.AddEdge([i,i+1]);
-  console.log(g.edgesList[i]);
-}
+// add the edges 
+Graph.AddEdge(0,1);
+Graph.AddEdge(0,2);
+Graph.AddEdge(1,3);
+Graph.AddEdge(1,4);
+Graph.AddEdge(2,5);
+Graph.AddEdge(2,6);
+Graph.AddEdge(3,7);
+Graph.AddEdge(3,8);
+Graph.AddEdge(4,9);
+Graph.AddEdge(4,10);
+
+// print the Graph
+console.log("graph print :");
+Graph.printGraph();
+
+
+
+// find starting points 
+console.log("startingpoints print :");
+const arr =Graph.FindStart();
+console.log("arr ="+arr);
+// find all layers 
+Graph.Looper(arr);
+
+
+
+
+
 
 
 
