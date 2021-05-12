8ecec135285a53821f4ce39864508234c14a56d8
BNOK <52835202+BNOK@users.noreply.github.com>
2021-05-12 05:22:03 +0100

    Update Prototype.js

--- JS files/Prototype.js
+++ JS files/Prototype.js
@@ -3,9 +3,32 @@ const fs = require('fs');
 GraphContent = JSON.parse(fs.readFileSync("./httpdata.json",{encoding : 'utf8'}));
 console.log(GraphContent);
 console.log("------------");
+let object = new Array();
 
-const vals = Object.values(GraphContent);
-const keys = Object.keys(GraphContent);
+// for(let i=0;i<vals.length;i++){
+//     console.log("keys :",i);
+//     console.log(Object.keys(vals[i]));
+//     console.log("vals :",i);
+//     console.log(Object.values(vals[i]));
+// }
 
-console.log(Object.values(vals[0]));
+//---- recursive function ---- 
 
+function props(obj){
+    for(let val in obj){
+        if (obj[val] === null){
+            
+            break;
+        }
+        else if(typeof(obj[val])==='object'){
+            
+            props(obj[val]);
+        }
+        else{
+            
+            console.log(`key : ${val}, value : ${obj[val]}`);
+        }
+    }
+}
+
+props(GraphContent)
