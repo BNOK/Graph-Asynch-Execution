623883d0f1aaa1df2e19a31be93fab43c2033111
BNOK <52835202+BNOK@users.noreply.github.com>
2021-05-18 12:59:57 +0100

    Update Prototype.js

--- JS files/Prototype.js
+++ JS files/Prototype.js
@@ -5,26 +5,44 @@ console.log(GraphContent);
 console.log("------------");
 let object = new Array();
 
-for(let val in GraphContent){
-    console.log("val:",val);
-    console.log("graph content",GraphContent[val]);
-}
+//-------------
+// let newobj ={}
+// for(let i=0;i<Object.values(GraphContent).length;i++){ 
+//     newobj[i] ={
+//         "key":Object.keys(GraphContent)[i],
+//         "value": Object.values(GraphContent)[i]
+//     };
+
+//     if (typeof(newobj[i])==='object'){
+//         console.log("this is an object",newobj[i]);
+//     }
+//     else{
+//         console.log("this is a prop",newobj[i])
+//     }
+
+// }
+
+// console.log("newobj:",Object.values(newobj)[1].value);
+
+// console.log("new obj:", newobj)
 
 
 //---- recursive function ---- 
-// function props(obj){
-//     for(let val in obj){
-//         if (obj[val] === null){
-//             break;
-//         }
-//         else if(typeof(obj[val])==='object'){
-//             console.log(`key : ${val}, value : ==>`);
-//             props(obj[val]);
-//         }
-//         else{
-//             console.log(`key : ${val}, value : ${obj[val]}`);
-//         }
-//     }
-// }
+function props(obj,index){
+    console.log("index :",index);
+    for(let val in obj){
+        if (obj[val] === null){
+            break;
+        }
+        else if(typeof(obj[val])==='object'){
+            console.log(`key : ${val}, value : ==>`);
+            index++;
+            props(obj[val],index);
+        }
+        else{
+            console.log(`key : ${val}, value : ${obj[val]}`);
+        }
+    }
+}
 
-// props(GraphContent)
+props(GraphContent,0)
